a34e5fc1664c754141b344c33a9441580fe1277a
Sebastian <sebfischer@gmx.net>
2019-11-12 12:11:13 +0100

    test

--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using Nuke.Common;
+using Nuke.Common.ChangeLog;
+using Nuke.Common.CI;
 using Nuke.Common.CI.AzurePipelines;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
@@ -9,12 +12,14 @@ using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.CoverallsNet;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitReleaseManager;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.ChangeLog.ChangelogTasks;
 
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
@@ -27,7 +32,7 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.PublishCoverage);
+    public static int Main () => Execute<Build>(x => x.Pack);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
@@ -36,8 +41,14 @@ class Build : NukeBuild
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
     [GitVersion] readonly GitVersion GitVersion;
+    [CI] readonly AzurePipelines AzurePipelines;
 
     AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    string ChangeLogFile => RootDirectory / "changelog.md";
+
+    IEnumerable<string> ChangeLogSectionNotes => ExtractChangelogSectionNotes(ChangeLogFile);
 
     Target Clean => _ => _
         .Before(Restore)
@@ -100,5 +111,20 @@ class Build : NukeBuild
                 ProcessTasks.StartProcess("codecov.exe", 
                     arguments: $"-f \"{ArtifactsDirectory / "coverage/coverage.cobertura.xml"}\" -t 8532ee50-6d63-4a1a-a36f-ad2741eb3e40");
             }
+        });
+
+    Target Pack => _ => _
+        .DependsOn(PublishCoverage)
+        .Produces(OutputDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetProject(Solution.GetProject("Datatent.Core"))
+                .SetNoBuild(true)
+                .SetConfiguration(Configuration)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetOutputDirectory(OutputDirectory)
+                .SetPackageReleaseNotes(GetNuGetReleaseNotes(ChangeLogFile, GitRepository))
+            );
         });
 }
